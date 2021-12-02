57c6702cf81a228c8233f73c1eae9ff26f91ef91
john morgangit config --global user.email jmm915@gmail.com <jmm915@gmail.com>
2021-12-02 15:32:27 -0600

    1st

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string dogName = "Ralph";
+int dogAge = 10;
+char middleInt = 'A';
+bool isAlive = true;
+double height = 2.2;
+decimal price = 5.10m;
+Console.WriteLine($"My dog's name  is {dogName} he is {dogAge}. His middle int is {middleInt}. It is {isAlive} that he is alive. He is {height} feet. The price is {price}.");
